2688b12ef728f6744e1cffa1d6884980456560df
YanaBaranova0909 <qa.yana.baranova@gmail.com>
2025-01-12 19:03:57 +0100

    Add index.js with chalk

---
+++ nodeBasics/index.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+
+console.log(chalk.green('This is a green text'));
+console.log(chalk.black('This is a black text'));
+console.log(chalk.greenBright.bold('This is a bright green and bold text'));
+
