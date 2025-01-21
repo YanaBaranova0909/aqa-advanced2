a580416d0a4b318bd734f0c3d8b0261253de3d42
YanaBaranova0909 <qa.yana.baranova@gmail.com>
2025-01-21 16:31:06 +0100

    Add jsBasics folder with tasks

---
+++ jsBasics/task3.js
@@ -0,0 +1,5 @@
+let age = 31;
+let isAdult = age >= 18;
+
+console.log("Age:", age);
+console.log("Is adult:", isAdult);
---
+++ jsBasics/task4.js
@@ -0,0 +1,15 @@
+let radiusCircle = 12.5;
+let circleArea = Math.PI * radiusCircle ** 2;          
+console.log("Area of the circle:", circleArea.toFixed(2));
+
+let length = 20;
+let width = 10;
+let rectangleArea = length * width;
+console.log("Area of the rectangle:", rectangleArea.toFixed(2));
+
+let radiusCylinder = 2;
+let height = 20;
+let cylinderVolume = Math.PI * (radiusCylinder ** 2) * height;      
+console.log("Volume of the cylinder:", cylinderVolume.toFixed(2));
+
+
